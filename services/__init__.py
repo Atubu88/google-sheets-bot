1bb576d3854daf25ad1e05c2a9310c322270e6bb
Atubu88 <165842809+Atubu88@users.noreply.github.com>
2025-12-08 17:01:48 +0100

    Handle user onboarding on /start

--- services/__init__.py
+++ services/__init__.py
@@ -1,1 +1,10 @@
 """Services package."""
+
+from .product_service import ProductService, Product
+from .user_service import UserService
+
+__all__ = [
+    "Product",
+    "ProductService",
+    "UserService",
+]
