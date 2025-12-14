5ff23e10e93e102692b950a1d0bf1ee896eabb45
Atubu88 <165842809+Atubu88@users.noreply.github.com>
2025-12-14 20:30:57 +0100

    Add settings service backed by SQLite

--- services/__init__.py
+++ services/__init__.py
@@ -2,11 +2,13 @@
 
 from .product_service import ProductService, Product
 from .promo_products import get_promo_products
+from .settings_service import SettingsService
 from .user_service import UserService
 
 __all__ = [
     "Product",
     "ProductService",
+    "SettingsService",
     "UserService",
     "get_promo_products",
 ]
